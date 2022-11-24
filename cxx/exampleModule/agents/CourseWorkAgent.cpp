f89b4c36c416acf1972be666b60fb1abe724da03
Perefurgon <91635546+p3r3furgon@users.noreply.github.com>
2022-11-24 22:27:32 +0300

    Problems with size counting

--- cxx/exampleModule/agents/CourseWorkAgent.cpp
+++ cxx/exampleModule/agents/CourseWorkAgent.cpp
@@ -26,39 +26,72 @@ namespace exampleModule
 // return startNode;
 // }
 
+void RemoveOldLinks(std::unique_ptr<ScMemoryContext> &ms_context, ScAddr& structure)
+{
+  ScIterator3Ptr i1 = ms_context->Iterator3(structure, ScType::EdgeAccessConstPosPerm, ScType::NodeConst);
+  while(i1->Next())
+  {
+   auto ourNode = i1->Get(2);
+   ScIterator3Ptr i2 = ms_context->Iterator3(ourNode, ScType::EdgeAccessConstPosPerm, ScType::LinkVar);
+   while(i2->Next())
+   {
+    ms_context->EraseElement(i2->Get(1));
+    ms_context->EraseElement(i2->Get(2));
+   }   
+  }  
+}
+
 void CreateDistancesLinks(std::unique_ptr<ScMemoryContext> &ms_context, ScAddr& structure){
   ScIterator3Ptr it3 = ms_context->Iterator3(structure, ScType::EdgeAccessConstPosPerm, ScType::NodeConst);
   while(it3->Next())
    {
-    ScAddr distanceLink = ms_context->CreateLink(ScType::LinkVar);
-    ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, it3->Get(2), distanceLink);
+    ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, it3->Get(2), ms_context->CreateLink(ScType::LinkVar));
    }   
 }
 
 ScAddr GetDistanceLink(std::unique_ptr<ScMemoryContext> &ms_context, ScAddr& node)
 {
-  ScAddr distanceLink;
   ScIterator3Ptr it3 = ms_context->Iterator3(node, ScType::EdgeAccessConstPosPerm, ScType::LinkVar);
   while(it3->Next())
    {
-    distanceLink = it3->Get(2);
+    return it3->Get(2);
    }   
-   return distanceLink;
+   return {};
 }
 
 uint32_t GetLinkInfo(std::unique_ptr<ScMemoryContext> &ms_context, ScAddr node)
 {
-  string strResult;
+  uint32_t res;
+  string strRes;
+   //SC_LOG_ERROR("1 keypoint");
   ScIterator3Ptr it3 = ms_context->Iterator3(node, ScType::EdgeAccessConstPosPerm, ScType::LinkVar);
-  SC_LOG_ERROR("1 keypoint");
   while(it3->Next())
    {
-    SC_LOG_ERROR("2 keypoint");
-    strResult = CommonUtils::getLinkContent(ms_context.get(), it3->Get(2));
+    //SC_LOG_ERROR("2 keypoint");
+    strRes = CommonUtils::getLinkContent(ms_context.get(), it3->Get(2));
    }
-   SC_LOG_ERROR("3 keypoint");   
-   uint32_t result = stoi(strResult);
-   return result;
+  //SC_LOG_ERROR("3 keypoint");   
+   res = stoi(strRes);
+   return res;
+}
+
+ScAddr GetMaxCycleSize(std::unique_ptr<ScMemoryContext> &ms_context, ScAddr distances)
+{
+  ScAddr res = ms_context->CreateNode(ScType::LinkVar);
+  uint32_t maxSize = 0;
+  uint32_t tmp;
+  ScIterator3Ptr iter = ms_context->Iterator3(distances, ScType::EdgeAccessConstPosPerm, ScType::LinkVar);
+  while(iter->Next())
+  {
+   auto strTmp = CommonUtils::getLinkContent(ms_context.get(), iter->Get(2));
+   tmp = stoi(strTmp); 
+   if (tmp > maxSize)
+   {
+    ms_context->SetLinkContent(res, to_string(tmp));
+    maxSize = tmp;
+   }
+  } 
+  return res; 
 }
 
 void DFS(ScLog *logger, std::unique_ptr<ScMemoryContext> &ms_context, ScAddr node, 
@@ -76,11 +109,11 @@ void DFS(ScLog *logger, std::unique_ptr<ScMemoryContext> &ms_context, ScAddr nod
     if(ms_context->HelperCheckEdge(visited, it3->Get(2), ScType::EdgeAccessConstPosPerm) && 
       !ms_context->HelperCheckEdge(globalVisited, it3->Get(2), ScType::EdgeAccessConstPosPerm))
     {
-      SC_LOG_ERROR("Cycle is found");
+      //SC_LOG_ERROR("Cycle is found");
       ScAddr cycleSize = ms_context->CreateNode(ScType::LinkVar);
-      //uint32_t value = GetLinkInfo(ms_context, node) - GetLinkInfo(ms_context, it3->Get(2))+1;
-      // ms_context->SetLinkContent(cycleSize, to_string(value));
-      // ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, distances, cycleSize);
+      uint32_t value = GetLinkInfo(ms_context, node) - GetLinkInfo(ms_context, it3->Get(2)) + (uint32_t)1;
+      ms_context->SetLinkContent(cycleSize, to_string(value));
+      ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, distances, cycleSize);
     } 
     else if(!ms_context->HelperCheckEdge(visited, it3->Get(2), ScType::EdgeAccessConstPosPerm))
     {
@@ -111,42 +144,38 @@ SC_AGENT_IMPLEMENTATION(CourseWorkAgent)
   ScAddr visited = ms_context->CreateNode(ScType::NodeConstClass);
   ScAddr globalVisited = ms_context->CreateNode(ScType::NodeConstClass);
   ScAddr distances = ms_context->CreateNode(ScType::NodeConstClass);
-  if (!param.IsValid()||!structure.IsValid())
+  if (!param.IsValid())
     return SC_RESULT_ERROR_INVALID_PARAMS;
 
   uint32_t tmpSize = 0;
-
-  CreateDistancesLinks(ms_context, structure);
 
   ScIterator3Ptr it3 = ms_context->Iterator3(structure, ScType::EdgeAccessConstPosPerm, ScType::NodeConst);
   while(it3->Next())
   {
    if(!ms_context->HelperCheckEdge(globalVisited, it3->Get(2), ScType::EdgeAccessConstPosPerm))
    {
-    ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, it3->Get(2));
+    RemoveOldLinks(ms_context, structure);
+    CreateDistancesLinks(ms_context, structure);
     DFS(logger, ms_context, it3->Get(2), visited, globalVisited, tmpSize, distances);
    }
   }   
 
-  ScIterator3Ptr i1 = ms_context->Iterator3(structure, ScType::EdgeAccessConstPosPerm, ScType::NodeConst);
-  while(i1->Next())
+  // ScAddr link = ms_context->CreateNode(ScType::LinkVar);
+  // uint32_t res;
+
+  // auto sp = CommonUtils::getLinkContent(ms_context.get(), link);
+  // uint32_t ip = stoi(sp);
+  // ip*=ip;
+  // ms_context->SetLinkContent(link, to_string(ip));
+  ScIterator3Ptr iter = ms_context->Iterator3(distances, ScType::EdgeAccessConstPosPerm, ScType::LinkVar);
+  while(iter->Next())
   {
-   auto ourNode = i1->Get(2);
-   ScIterator3Ptr i2 = ms_context->Iterator3(ourNode, ScType::EdgeAccessConstPosPerm, ScType::LinkVar);
-   while(i2->Next())
-   {
-    SC_LOG_ERROR("Node has link");
-   }   
-  }   
-
-
-  // ScIterator3Ptr iter = ms_context->Iterator3(distances, ScType::EdgeAccessConstPosPerm, ScType::LinkVar);
-  // while(iter->Next())
-  // {
-  //  auto linka = iter->Get(2);
-  //  logger->Message(ScLog::Type::Info, "res "+ CommonUtils::getLinkContent(ms_context.get(), linka));
-  // }  
-
+   auto linka = iter->Get(2);
+   logger->Message(ScLog::Type::Info, "cycle size "+ CommonUtils::getLinkContent(ms_context.get(), linka));
+  }  
+  ScAddr result = GetMaxCycleSize(ms_context, distances);
+  ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, result);
+  logger->Message(ScLog::Type::Info, "Graph circumference:  "+ CommonUtils::getLinkContent(ms_context.get(), result));
   SC_LOG_ERROR("testAgent finished");
   utils::AgentUtils::finishAgentWork(ms_context.get(), questionNode, answer);
   return SC_RESULT_OK;
